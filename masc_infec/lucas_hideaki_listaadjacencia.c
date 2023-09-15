43f03835636d9d0d9483c008bc5d63130c459142
hideaki-san <lucashideaki07@gmail.com>
2023-09-15 20:46:17 -0300

    ajeitado a adjacencia

--- masc_infec/lucas_hideaki_listaadjacencia.c
+++ masc_infec/lucas_hideaki_listaadjacencia.c
@@ -11,29 +11,81 @@ struct graph *next;
 struct list
 {
 struct graph **adj;
-int size;
+char buffer_vertex[10];
+int size_vertex;
+int size_edge;
 }list;
 
 
-struct list *listInit(int lines);
+struct list *listInit(char *name_arq);
 struct graph *graphAdd(char individuo, int estado);
+
 int num_lines(char *name_arq);
-void graphPopulate(struct list **L, FILE *arq);
+void graphPopulate(struct list **L, char *name_arq);
 void graphPrint(struct list *L);
 void conditional(FILE *arq, int a, int b);
 void graphTxtGen(struct list *L);
 void listFree(struct list **L);
 
 
-struct list *listInit(int lines)
+struct list *listInit(char *name_arq)
 {
-  struct list *L =(struct list *)calloc(1, sizeof(struct list));
-  L->adj = (struct graph **)malloc(lines * sizeof(struct graph *));
+  int lines = num_lines(name_arq);
 
-  for(int n = 0; n < lines; n++)
-    L->adj[n] = NULL;
+  FILE *arq = fopen(name_arq, "r");
+
+  if(arq == NULL)
+    return 0;
+
+  struct list *L =(struct list *)calloc(1, sizeof(struct list));
   
-  L->size = lines;
+  int vertex_controller = 0, add_flag = 0;
+  char c = fgetc(arq);
+
+  do{
+
+    if((c != ' ') && (c != '\n') && (c != ','))
+    {
+      for(int n = 0; n < vertex_controller; n++)
+      {
+          if(L->buffer_vertex[n] == c)
+          {
+              add_flag = 1;
+              n = vertex_controller;
+          }
+      }
+
+      if(add_flag == 0)
+      {
+          L->buffer_vertex[vertex_controller] = c;
+          L->size_vertex++;
+          vertex_controller++;
+      }
+
+      add_flag = 0;
+    }
+
+    c = fgetc(arq);
+
+  }while(c != EOF);
+
+  L->size_vertex = L->size_vertex/2;
+  L->adj = (struct graph **)malloc(L->size_vertex * sizeof(struct graph *));
+
+  for(int n = 0; n < L->size_vertex; n++)
+    L->adj[n] =(struct graph *)malloc(sizeof(struct graph));
+
+  vertex_controller = 0;
+
+  for(int n = 0; n < L->size_vertex; n++)
+  {
+    L->adj[n] = graphAdd(L->buffer_vertex[vertex_controller], L->buffer_vertex[vertex_controller+1]);
+    vertex_controller += 2;
+  }
+
+  L->size_edge = num_lines(name_arq);
+
+  fclose(arq);
   return L;
 }
 
@@ -47,18 +99,15 @@ struct graph *graphAdd(char individuo, int estado){
 return G;
 }
 
-int num_lines(char *name_arq){
+int num_lines(char *name_arq)
+{  
+  FILE *arq = fopen(name_arq, "r");
+
+  if(arq == NULL)
+    return 0;
   
-  int lines=0;
-  char line_complete[12];  
-
-  FILE *arq;
-  arq = fopen(name_arq, "r");
-
-  if(arq == NULL){
-    printf("erro..\n");
-    return 1;
-  } 
+  int lines = 0;
+  char line_complete[12];   
 
   while(fgets(line_complete, sizeof(line_complete), arq))
     lines++;
@@ -68,50 +117,52 @@ int num_lines(char *name_arq){
 return lines;
 }
 
-
-void graphPopulate(struct list **L, FILE *arq){
-   
+void graphPopulate(struct list **L, char *name_arq)
+{
   if(L == NULL || (*L)->adj == NULL)
     return;
-  
-  struct graph **aux = (*L)->adj, *ref;
-  
-  char line_complete[1024], buff[2], c;
-  int character_pos = 0, buff_controller = 0;
-  int line_controller = 0, new_insertion = 0;
- 
-  while(fgets(line_complete, sizeof(line_complete), arq)){      
-    c = line_complete[character_pos];
 
-    do{
+  FILE *arq = fopen(name_arq, "r");  
 
-      if(c != ',' && c != ' '){
-        buff[buff_controller] = c;
-        buff_controller++;
+  if(arq == NULL)
+    return;
 
-        if(buff_controller > 1 && new_insertion == 0){
-          aux[line_controller] = graphAdd(buff[0], buff[1]);
-          ref = aux[line_controller];
-          buff_controller = 0;
-          new_insertion = 1;
+  struct graph **aux = (*L)->adj, *G, *g, *G_ref, *g_ref;
+  char c = fgetc(arq), ref;  
+
+  do{
+    if((c != ' ' && c != ',') && (c != '\n'))
+    {
+      for(int v = 0; v < (*L)->size_vertex; v++)
+      {
+        G = aux[v];
+        if(G->individuo == c)
+        {
+          c = fgetc(arq); ///,
+          c = fgetc(arq); //int
+          c = fgetc(arq); //,
+          c = fgetc(arq); //char
+
+          G = aux[v];
+          g = G;
+          ref = c;
+
+          while(g->next != NULL)
+            g = g->next;
+          
+          c = fgetc(arq); //,
+          c = fgetc(arq); //int
+          
+          g->next = graphAdd(ref, c);
+
+          v = (*L)->size_vertex;
         }
-
-        if(buff_controller > 1 && new_insertion == 1){
-          ref->next = graphAdd(buff[0], buff[1]);
-          ref = ref->next;
-          buff_controller = 0;
-        }   
       }
-
-      character_pos++;
-      c = line_complete[character_pos]; 
-
-    }while(c != '\n' && c != '\0');
-  
-    line_controller++;
-    new_insertion = 0;
-    character_pos = 0;
     }
+
+    c = fgetc(arq);
+  }while(c != EOF);
+
 }
 
 void graphPrint(struct list *L){
@@ -119,40 +170,50 @@ void graphPrint(struct list *L){
   if(L == NULL || L->adj == NULL)
     return;
 
-  struct graph *g;
+  struct graph *G = L->adj[0], *g;
 
-  for(int n = 0; n < L->size; n++){
+  for(int n = 0; n < L->size_vertex; n++){
+    
     g = L->adj[n];
+
+    printf("Vertivce:[%c]: ",g->individuo);
+
+    g = g->next;
+
+    if(g == NULL)
+      printf("NULL pois nao eh referencia para infeccao");
+
     while(g != NULL){
       printf("%c,%d  ", g->individuo, g->estado);
       g = g->next;
     }
+
   printf("\n");
   }
 }
 
 /*
-1 - Sem máscara, sem infecção da COVID-19
-2 - Com máscara, sem infecção da COVID-19
-3 - Com máscara, com infecção da COVID-19
-4 - Sem máscara, com infecção da COVID-19
+0 - Sem máscara, sem infecção da COVID-19
+1 - Com máscara, sem infecção da COVID-19
+2 - Com máscara, com infecção da COVID-19
+3 - Sem máscara, com infecção da COVID-19
 */
 
 void conditional(FILE *arq, int a, int b){
 
-  if((a == 1 && b == 1)||(a == 1 && b == 2)||(a == 2 && b == 1)||(a == 2 && b == 2)){
+  if((a == 0 && b == 0)||(a == 0 && b == 1)||(a == 1 && b == 0)||(a == 1 && b == 1)){
     fprintf(arq, "== 0%%");
   } 
   
-  if((a == 2 && b == 3) || (a == 3 && b == 2)){
+  if((a == 1 && b == 2) || (a == 2 && b == 1)){
     fprintf(arq, "== 25%%");
   }
 
-  if(((a == 1 && b == 3) || (a== 3 && b == 1)) || ((a == 2 && b == 4) || (a == 4 && b == 2))){
+  if(((a == 0 && b == 2) || (a== 2 && b == 0)) || ((a == 1 && b == 3) || (a == 3 && b == 1))){
     fprintf(arq, "== 50%%");
   }
 
-  if((a == 1 && b == 4) || (a == 4 && b == 1)){
+  if((a == 0 && b == 3) || (a == 3 && b == 0)){
     fprintf(arq, "== 100%%");
   }
 }
@@ -167,30 +228,24 @@ void graphTxtGen(struct list *L)
   if(txt == NULL)
     return;
 
+  fprintf(txt,"[0]Sem máscara, sem infecção da COVID-19\n[1]Com máscara, sem infecção da COVID-19\n[2]Com máscara, com infecção da COVID-19\n[3]Sem máscara, com infecção da COVID-19\n\n");
   
-  fprintf(txt, "Significado das condições:\n");
-  fprintf(txt,"[1]Sem máscara, sem infecção da COVID-19\n[2]Com máscara, sem infecção da COVID-19\n[3]Com máscara, com infecção da COVID-19\n[4]Sem máscara, com infecção da COVID-19\n\n");
-  
-  struct graph *g;
+  struct graph *G, *g;
   int state[2], state_controller = 0;
 
-  for(int n = 0; n < L->size; n++){
-    g = L->adj[n];
-    while(g != NULL){
-      fprintf(txt, "%c,%d  ", g->individuo, g->estado);
+  for(int n = 0; n < L->size_vertex; n++){
+    G = L->adj[n];
+    g = G->next;
 
-      if(state_controller < 2){
-        state[state_controller] = g->estado;
-        state_controller++;
-      }
+    while(g != NULL){
+      fprintf(txt, "VERTICE ANALISADO [%c|%d]: ", G->individuo, G->estado);
+      fprintf(txt, "%c,%d  ", g->individuo, g->estado);
+      conditional(txt, g->estado, G->estado);
+      fprintf(txt, "\n");
 
       g = g->next;
     }
-  if(state_controller == 2)
-    conditional(txt, state[0], state[1]);
-  
-  state_controller = 0;
-  fprintf(txt, "\n");
+
   }
 
 fclose(txt);
@@ -199,9 +254,12 @@ fclose(txt);
 void listFree(struct list **L)
 {
   struct graph **aux = (*L)->adj;
-  struct graph *ref = (*L)->adj[0], *ref2;
+  struct graph *ref, *ref2;
 
-  for(int n=0; n < (*L)->size; n++){
+  for(int n=0; n < (*L)->size_vertex; n++)
+  {
+    ref = aux[n];
+    
     while(ref != NULL){
       ref2 = ref;
       ref = ref->next;
@@ -222,22 +280,12 @@ int main(){
 
   char name_arq[20] = "banco.csv";
 
-  int lines = num_lines(name_arq);
+  struct list *L = listInit(name_arq);
+  graphPopulate(&L, "banco.csv");
 
-  struct list *L = listInit(lines);
-  
-  FILE *arq = fopen(name_arq, "r");
-  
-  if(arq == NULL){
-    printf("erro.\n");
-    return 0;
-  }
-
-  graphPopulate(&L, arq);
-  fclose(arq);
+  graphTxtGen(L);
   
   graphPrint(L);
-  graphTxtGen(L);
   listFree(&L);
 
 return 0;   
